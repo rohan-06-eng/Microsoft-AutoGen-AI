17e92d0060288a74d6a891f2fbd8a3febea7a638
Rohan Raj <rohan69raj03@gmail.com>
2025-07-25 05:32:14 +0000

    Added the Async_Run Implementation - 25/07/2025

--- AutoGen_AgentChat/2_Async_Run.py
+++ AutoGen_AgentChat/2_Async_Run.py
@@ -1,28 +1,27 @@
 import asyncio
 import time
 
-async def brew_Hot_Choclate():
-    print("Starting Brewing Hot Choclate")
+async def make_hot_chocolate():
+    print("Starting to make Hot Chocolate")
     await asyncio.sleep(3)
-    print("Hot Choclate Ready")
+    print("Hot Chocolate is ready")
 
-async def toast_Toast():
-    print("Start Toasting Toast")
+async def toast_bread():
+    print("Starting to Toast Bread")
     await asyncio.sleep(2)
-    print("Toast Ready")
+    print("Toasted Bread is ready")
 
 
 async def main():
     start = time.time()
 
-    coffee = brew_Hot_Choclate()
-    toast = toast_Toast()
+    hot_chocolate = make_hot_chocolate()
+    toasted_bread = toast_bread()
 
-    results = await asyncio.gather(coffee,toast)
+    results = await asyncio.gather(hot_chocolate, toasted_bread)
 
     end = time.time()
 
-    print(f"Time : {end - start:.2f} minutes")
-
+    print(f"Time: {end - start:.2f} minutes")
 
 asyncio.run(main())
